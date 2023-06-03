8d958f28076bc868a8212fae186a94a8059ee1c9
RubberY123 <67812260+RubberY123@users.noreply.github.com>
2023-06-03 10:23:57 +0800

    Update SQLQuery.sql
    A brief process and analyze steps using SQL for Google Case Study Challenge 1

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -62,6 +62,21 @@ from A
 where number_of_rides > 1;
 --This shows that each id represents unique rides
 
+--Check for NULL datas
+Select member_casual, start_station_name, count(member_casual) As Total
+from dbo.Tripdata_2022
+Group by member_casual, start_station_name order by Total desc
+-- It is absurd that there are almost 100,000 data missing. We could choose to delete the incomplete data or fill it manually using the data from start_lat and start_lng as below.
+
+--From the start_lat and start_lng we will find out the start_station_name and id
+Select start_station_name from dbo.tripdata_2022
+where start_lat like 41.93 and start_lng like -87.76 and start_station_name is not null
+--We can observe that start_lat and start_lng is not a good indicator for us to fill up the null data as start_station_name has 
+--different name such as Public Rack and Lockwood Ave for the same start_lat and start_lng figures.
+--Therefore, we could consider removing the null datas
+
+
+
 --Usage time for all bikes
 With B (rideable_type, started_at, ended_at, Total_Travel_Time) AS 
 (Select rideable_type, started_at, ended_at, datediff(MINUTE, started_at, ended_at) as Total_Travel_Time
@@ -70,9 +85,15 @@ Select rideable_type, AVG(Total_Travel_Time) as Average_Travel_Time
 FROM B
 Group By rideable_type
 Order by 2 DESC;
---We could observe that docked bike is used for the longest travel time.
+--We could observe that docked bike is used for the longest average travel time.
 
 --Check for number of member and casual
+Select member_casual, rideable_type, count(rideable_type) As Total_Count
+from dbo.Tripdata_2022
+group by member_casual, rideable_type
+--Shows that most of the riders are members and members do not used docked bikes.
+
+--Lets observe the average travel time for each riders
 WITH C (member_casual, rideable_type, Total_Travel_Time) AS (Select member_casual, rideable_type, datediff(MINUTE, started_at, ended_at) as Total_Travel_Time
 FROM dbo.Tripdata_2022)
 SELECT member_casual, rideable_type, AVG(Total_Travel_Time) AS Average_Time
@@ -91,13 +112,13 @@ Total FLOAT)
 
 INSERT INTO Station_Total_2022
 Select start_station_name, member_casual, count(start_station_name) As Total
-from dbo.Tripdata_202212
+from dbo.Tripdata_2022
 Group by start_station_name, member_casual
 
+Select * from Station_Total_2022 order by 3 desc
 
-Select * 
-from Station_Total_2022
-Order by 2, 3 DESC;
+--This table shows the top destination used by our riders as starting destination. Since our data include latitude and longtitude data, 
+--it would be better to observe the data using mapping analysis (Ex. Tableau).
 
 DROP TABLE IF exists Member_Total_2022
 Create Table Member_Total_2022(
@@ -112,30 +133,25 @@ Group by member_casual
 Select * 
 from Member_Total_2022
 
+-- Create a table for showing the number of riders in member and casual category.
+
 Select Station_Total_2022.start_station_name, Station_Total_2022.member_casual, 
 (Station_Total_2022.Total/Member_Total_2022.Total_Member) * 100 AS Percentage_of_Member
 from Station_Total_2022
 Join Member_Total_2022
 ON Station_Total_2022.member_casual = Member_Total_2022.member_casual
 Order by 3 DESC;
---Based on the observation, we could see that most of annual members started their destination from Clark St, Canal St, University Ave and Ellis Ave. 
---While most of the casual bikers started their destination from Streeter Dr. However, we could also observe that most of our bikers started their
---destination from Clark St. We shall look into the top 5 starting destination from our bikers.
 
-Select member_casual, start_station_name, count(member_casual) As Total
-from dbo.Tripdata_2022
-Group by member_casual, start_station_name order by Total desc
--- It is absurd that there are almost 100,000 data missing
---
+--The above shows the percentage of riders starting destination. For example, we could observe that 1.43% of the casual riders starts their destination from Michigan Ave & Oat St.
+--While 0.95% of annual members starts their destination from starts their destination from Loomis St & Lexington St. 
+--We could suggest to promote the membership of Cyclist in the top starting destination by casual members. 
 
---from the start_lat and start_lng we will find out the start_station_name and id
-Select start_station_name from dbo.tripdata_2022
-where start_lat like 41.93 and start_lng like -87.76 and start_station_name is not null
---We can observe that start_lat and start_lng is not a good indicator for us to fill up the null data as start_station_name has 
---different name such as Public Rack and Lockwood Ave for the same start_lat and start_lng figures.
---Therefore, we could consider removing the null datas
 
---Added new column Duration
+
+
+--Alter table by adding new column
+
+--Added new column Duration, only need to run once
 ALTER TABLE Tripdata_2022 ADD Duration FLOAT
 UPDATE Tripdata_2022 
 Set Duration = datediff(hh, started_at, ended_at)
@@ -143,4 +159,4 @@ Set Duration = datediff(hh, started_at, ended_at)
 Select * from dbo.Tripdata_2022
 order by Duration DESC
 
---We have to check why there are no end station recorded, might it be bike lost during cycle? As most riders are casual members
+--We have to check why there are no end station recorded, might it be bike lost during cycle? As most riders that travels long distance are casual members.
